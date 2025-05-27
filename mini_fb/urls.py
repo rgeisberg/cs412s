202ab768397f0c1de8afd3955d03134bd1db404c
Becky Geisberg <rgeis26@bu.edu>
2025-05-27 11:14:41 -0400

    done

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,8 +2,9 @@
 # Author: Becky Geisberg, (rgeis26@bu.edu)
 # Description: url file for mini_fb
 from django.urls import path
-from .views import ShowAllProfilesView
+from .views import ShowAllProfilesView, ShowProfile
 
 urlpatterns = [
     path('',ShowAllProfilesView.as_view(), name='show_all_profiles'),
+    path('profile/<int:pk>', ShowProfile.as_view(), name="profile"),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -2,8 +2,7 @@
 # Author: Becky Geisberg, (rgeis26@bu.edu)
 # Description: views file for mini_fb
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView 
-from django.views.generic import ListView, DetailView 
+from django.views.generic import ListView, DetailView  
 from .models import Profile
 
 # Create your views here.
@@ -13,5 +12,11 @@ class ShowAllProfilesView(ListView):
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
+
+class ShowProfile(DetailView):
+    """Defind a view class to show one profile"""
+    model = Profile
+    template_name = 'mini_fb/show_profile.html'
+    context_object_name = 'profile'
 
 
