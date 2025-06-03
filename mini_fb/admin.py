2b80dff4a05d488c153384e1eccd4b5b09460a92
Becky Geisberg <rgeis26@bu.edu>
2025-06-03 10:26:45 -0400

    task 1

--- mini_fb/admin.py
+++ mini_fb/admin.py
@@ -2,10 +2,12 @@
 # Author: Becky Geisberg, (rgeis26@bu.edu)
 # Description: admin file for mini_fb
 from django.contrib import admin
-from .models import Profile, StatusMessage
+from .models import Profile, StatusMessage, Image, StatusImage
 
 # Register your models here.
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(StatusMessage)
+admin.site.register(Image)        
+admin.site.register(StatusImage)
--- mini_fb/models.py
+++ mini_fb/models.py
@@ -24,6 +24,7 @@ class Profile(models.Model):
           """return all status messages for a particular user"""
           status_messages = StatusMessage.objects.filter(profile=self).order_by('-timestamp')
           return status_messages
+    
     def get_absolute_url(self):
         """display new profile"""
         return reverse('profile', kwargs={'pk':self.pk})
@@ -41,5 +42,33 @@ class StatusMessage(models.Model):
             """return a string representation of this model"""
             return f'{self.message} made at {self.timestamp} by {self.profile}'
     
+    def get_images(self):
+        """acessor method for all images """
+        images = StatusImage.objects.filter(status_message=self)
+        return images
+
+
+    
+class Image(models.Model):
+      """Encapsulate the data of an  image """
+      profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+      image_file = models.ImageField(blank=True)
+      timestamp = models.DateTimeField(auto_now=True)
+      caption = models.TextField(blank=True)
+
+      def __str__(self):
+        return f"Image for {self.profile.firstName}"
+
+      # make the form?? with meta data if not asked to later
+
+class StatusImage(models.Model):
+      """Encapsulate the relationship between images and status messages"""
+      image = models.ForeignKey("Image", on_delete=models.CASCADE)
+      status_message = models.ForeignKey("StatusMessage", on_delete=models.CASCADE)
+
+      def __str__(self):
+        return f"image plus {self.status_message}"
+
+
 
 
