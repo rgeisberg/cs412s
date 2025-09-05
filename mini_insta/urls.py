067ffd15ee44d4c16bb3f57dc04df9b249f8d1cc
Becky Geisberg <rgeis26@bu.edu>
2025-09-05 11:53:26 -0400

    finished assignment 3

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import *
+urlpatterns = [
+    path('', show_all_profiles.as_view(), name='show_all_profiles'),
+    path('profile/<int:pk>', profile_detail_view.as_view(), name="profile")
+]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import *
 
 # Create your views here.
@@ -8,3 +8,9 @@ class show_all_profiles(ListView):
     model = Profile
     template_name = 'mini_insta/show_all_profiles.html'
     context_object_name = 'profiles'
+
+class profile_detail_view(DetailView):
+    """Define a view class to show all profiles"""
+    model = Profile
+    template_name = 'mini_insta/show_profile.html'
+    context_object_name = 'profile'
